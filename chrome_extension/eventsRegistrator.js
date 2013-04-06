9e668c67298ead6b6ccd9ee261a10a2c17877414
petercipov <petercipov@gmail.com>
2013-04-06 15:57:41 +0200

    - added nicer theme :)
    - dialog is centered

--- chrome_extension/eventsRegistrator.js
+++ chrome_extension/eventsRegistrator.js
@@ -2,4 +2,20 @@ $('body').mouseup(function(e) {
 	if (! e.ctrlKey) { return; }
 	var text = window.getSelection().toString();
   	console.log(text);
+
+  	$('#liberator_map_container').text(text);
+  	$('#liberator_map_container').dialog('open');
+  	$('#liberator_map_container').dialog("option", "position", { my: "center", at: "center", of: window });
+
 });
+
+
+$('body').append('<div id="liberator_map_container"></div>');
+$('#liberator_map_container').dialog(
+		{
+			title: "In place map",
+			width: 400,
+			height: 400,
+			autoOpen: false
+		}
+	);
